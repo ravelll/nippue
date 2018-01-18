4c5d93f4d9c95f9dfc4d740a526fb1cc24cc3741
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2018-01-18 16:00:35 +0900

    Add login view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Hello from '@/views/Hello.vue'
 import PostList from '@/views/PostList.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -14,6 +15,9 @@ export default new VueRouter({
     },
     {
       path: '/posts', component: PostList
+    },
+    {
+      path: '/login', component: Login
     }
   ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <div>User ID</div>
+      <input type="text" v-model="userId">
+    </div>
+    <div>
+      <div>Password</div>
+      <input type="password" v-model="password">
+    </div>
+    <input type="button" value="submit" @click.prevent="login">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userId: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      console.log("do login process")
+    }
+  }
+}
+</script>
