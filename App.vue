3f7c7d442240fa54e97516ea609a2693241cc13a
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2018-01-18 12:07:23 +0900

    hello! with vue-router

--- App.vue
+++ App.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="app">
-    Hello!
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'app'
-}
-</script>
---
+++ components/Hello.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script>
+export default {
+  data () {
+    return { text: 'Hello!' }
+  }
+}
+</script>
