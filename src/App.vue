77bacca232611674123b644a96c498a3d525d0e5
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2018-09-07 23:30:10 +0900

    Add general styles

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,36 @@
     <router-view></router-view>
   </div>
 </template>
+
+<style lang="scss">
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.4rem;
+  font-weight: normal;
+  font-family: 'ヒラギノ角ゴ ProN W3', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'Yu Gothic', YuGothic, Verdana, Helvetica, 'ＭＳ Ｐゴシック', sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  margin: 0;
+}
+
+a {
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+}
+</style>
