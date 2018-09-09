7e87d71dbb6a36582857ad39c744a3f11d059c23
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2018-09-09 17:03:15 +0900

    fix Login page design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,27 @@
 <template>
-  <div>
-    <div>
-      <div>User ID</div>
-      <input type="text" v-model="userId">
-    </div>
-    <div>
-      <div>Password</div>
-      <input type="password" v-model="password">
-    </div>
-    <input type="button" value="submit" @click.prevent="login">
-    <div v-if="errorMessage">
-      <p>{{ errorMessage }}</p>
+  <div class="login-block">
+    <div class="login-forms">
+      <div class="user-block">
+        <div class="form-label">User ID</div>
+        <el-input class="input" type="text" v-model="userId"/>
+      </div>
+      <div class="password-block">
+        <div class="form-label">Password</div>
+        <el-input class="input" type="password" v-model="password"/>
+      </div>
+      <div class="submit-block">
+        <el-button>Login</el-button>
+      </div>
+      <div v-if="errorMessage">
+        <p>{{ errorMessage }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Login',
   data () {
     return {
       userId: '',
@@ -38,3 +43,29 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.login-block {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.password-block {
+  padding: 2.4rem 0 4rem;
+}
+
+.form-label {
+  color: #8a8a8a;
+}
+
+.input {
+  width: 30rem;
+}
+
+.submit-block {
+  display: flex;
+  justify-content: center;
+}
+</style>
